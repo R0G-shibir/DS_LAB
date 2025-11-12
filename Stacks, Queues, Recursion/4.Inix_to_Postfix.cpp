a5e1741c6217b6046666a448b56386434af1dfac
Fuad <sbakul420@gmail.com>
2025-11-13 05:46:58 +0600

    infix to postfix

--- Stacks, Queues, Recursion/4.Inix_to_Postfix.cpp
+++ Stacks, Queues, Recursion/4.Inix_to_Postfix.cpp
@@ -20,7 +20,50 @@ int Precedence(char c){
     }
 }
 
+bool IsOperator(char c){
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+
+string Infix_to_Postfix(string infix){
+    stack<char>s;
+    string postfix = "";
+
+    s.push('(');
+    infix += ')';
+    for(int i=0;i<infix.length();i++){
+        if(infix[i] == '('){
+            s.push(infix[i]);
+        }
+        else if(infix[i] == ')'){
+            while(!s.empty() && s.top() != '('){
+                postfix += s.top();
+                s.pop();
+            }
+            s.pop();
+        }
+        else if(!IsOperator(infix[i])){
+            postfix += infix[i];
+        }
+        else if(IsOperator(infix[i])){
+            while(!s.empty() && 
+                ((Precedence(infix[i]) < Precedence(s.top())) || 
+                (Precedence(infix[i]) == Precedence(s.top()) && infix[i] != '^'))){
+                    postfix += s.top();
+                    s.pop();
+            }
+            s.push(infix[i]);
+        }
+    }
+    return postfix;
+}
+
+
 int main(){
 
+    cout<<"Enter Infix Expression : ";
+    string infix;cin>>infix;
+
+    cout << "Postfix Expression: " << Infix_to_Postfix(infix) << endl;
+    return 0;
 
 }
