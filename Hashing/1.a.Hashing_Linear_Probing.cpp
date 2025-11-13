e939e7a087d4388ecf840063604ad32427a59e1f
Fuad <sbakul420@gmail.com>
2025-11-14 02:12:45 +0600

    started Hashing

---
+++ Hashing/1.a.Hashing_Linear_Probing.cpp
@@ -0,0 +1,59 @@
+/*
+    Write a program to store some data 
+    (54, 26, 93, 17, 77, 31, 44, 55, 20) 
+    to a hash table (Size of hash table is a prime number).
+    While storing data if there is a collision 
+    use the Linear Probing technique
+
+*/
+
+#include <iostream>
+#define TABLE_SIZE 11
+using namespace std;
+
+int Hash_Table[TABLE_SIZE];
+
+void init_Table(){
+    for(int i =0;i<TABLE_SIZE;i++){
+        Hash_Table[i] = -1;
+    }
+}
+
+int hash_Function(int key){
+    return key % TABLE_SIZE;
+}
+
+void insert(int key){
+    int index = hash_Function(key);
+    int startIndex = index ;
+
+    while(Hash_Table[index] != -1){
+        index = (index + 1) % TABLE_SIZE;
+        if(startIndex == index){
+            cout << "Hash table is full! Cannot insert " << key << endl;
+            return ;
+        }
+    }
+    Hash_Table[index] = key;
+}
+
+void display(){
+    cout<<"\nIndex  \tValue\n";
+    for(int i = 0;i<TABLE_SIZE;i++){
+        cout<<"   "<<i<<"\t  "<<Hash_Table[i]<<endl;
+    }
+}
+
+int main(){
+
+    int data[] = { 54, 26, 93, 17, 77, 31, 44, 55, 20};
+    int size = sizeof(data) / sizeof(data[0]);
+    init_Table();
+
+    for(int i = 0;i<size;i++){
+        insert(data[i]);
+    }
+
+    display();
+    return 0;
+}
